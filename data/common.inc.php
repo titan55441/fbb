a1c77f9a490b6b2d5bac76e55b1e20884258c59c
titanwayne <titan55441@gmail.com>
2019-01-10 09:00:15 +0800

    Initial commit

---
+++ data/common.inc.php
@@ -0,0 +1,12 @@
+<?php
+//数据库连接信息
+$cfg_dbtype = 'mysql';
+$cfg_dbhost = 'localhost';
+$cfg_dbname = 'dedecmsv57utf8sp204';
+$cfg_dbuser = 'root';
+$cfg_dbpwd = 'mocca2966';
+$cfg_dbprefix = 'dede_';
+$cfg_db_language = 'utf8';
+
+
+?>
---
+++ data/config.cache.inc.php
@@ -0,0 +1,185 @@
+<?php
+$cfg_disable_funs = 'phpinfo,eval,exec,passthru,shell_exec,system,proc_open,popen,curl_exec,curl_multi_exec,parse_ini_file,show_source,file_put_contents';
+$cfg_disable_tags = 'php';
+$cfg_basehost = 'http://dev04.com';
+$cfg_cmspath = '';
+$cfg_cookie_encode = 'xiK6kWGNyd1mOggnmDOHCHKc05SG';
+$cfg_indexurl = '/';
+$cfg_backup_dir = 'backupdata';
+$cfg_indexname = '主页';
+$cfg_webname = '南通富宝贝生态农业有限公司';
+$cfg_adminemail = 'admin@dedecms.com';
+$cfg_html_editor = 'ckeditor';
+$cfg_arcdir = '/a';
+$cfg_medias_dir = '/uploads';
+$cfg_ddimg_width = 240;
+$cfg_ddimg_height = 180;
+$cfg_domain_cookie = '';
+$cfg_imgtype = 'jpg|gif|png';
+$cfg_softtype = 'zip|gz|rar|iso|doc|xsl|ppt|wps';
+$cfg_mediatype = 'swf|mpg|mp3|rm|rmvb|wmv|wma|wav|mid|mov';
+$cfg_specnote = 6;
+$cfg_list_symbol = ' > ';
+$cfg_notallowstr = '非典|艾滋病|阳痿';
+$cfg_replacestr = '她妈|它妈|他妈|你妈|去死|贱人';
+$cfg_feedbackcheck = 'N';
+$cfg_keyword_replace = 'Y';
+$cfg_fck_xhtml = 'N';
+$cfg_df_style = 'shengtainongye';
+$cfg_multi_site = 'N';
+$cfg_dede_log = 'N';
+$cfg_powerby = 'Copyright &copy; 2002-2017 DEDECMS. 织梦科技 版权所有';
+$cfg_arcsptitle = 'N';
+$cfg_arcautosp = 'N';
+$cfg_arcautosp_size = 5;
+$cfg_auot_description = 240;
+$cfg_ftp_host = '';
+$cfg_ftp_port = 21;
+$cfg_ftp_user = '';
+$cfg_ftp_pwd = '';
+$cfg_ftp_root = '/';
+$cfg_ftp_mkdir = 'N';
+$cfg_feedback_ck = 'Y';
+$cfg_list_son = 'Y';
+$cfg_mb_open = 'N';
+$cfg_mb_album = 'Y';
+$cfg_mb_upload = 'Y';
+$cfg_mb_upload_size = 1024;
+$cfg_mb_sendall = 'Y';
+$cfg_mb_rmdown = 'Y';
+$cfg_cli_time = 8;
+$cfg_mb_addontype = 'swf|mpg|mp3|rm|rmvb|wmv|wma|wav|mid|mov|zip|rar|doc|xsl|ppt|wps';
+$cfg_mb_max = 500;
+$cfg_keyword_like = 'N';
+$cfg_index_max = 10000;
+$cfg_index_cache = 86400;
+$cfg_tplcache = 'Y';
+$cfg_tplcache_dir = '/data/tplcache';
+$cfg_makesign_cache = 'N';
+$cfg_rm_remote = 'Y';
+$cfg_arc_dellink = 'N';
+$cfg_arc_autopic = 'Y';
+$cfg_arc_autokeyword = 'Y';
+$cfg_title_maxlen = 60;
+$cfg_album_width = 800;
+$cfg_check_title = 'Y';
+$cfg_album_row = 3;
+$cfg_album_col = 4;
+$cfg_album_pagesize = 12;
+$cfg_album_style = 2;
+$cfg_album_ddwidth = 200;
+$cfg_mb_notallow = 'www,bbs,ftp,mail,user,users,admin,administrator';
+$cfg_mb_idmin = 3;
+$cfg_mb_pwdmin = 3;
+$cfg_mb_pwdtype = '32';
+$cfg_md_idurl = 'N';
+$cfg_mb_rank = 10;
+$cfg_feedback_time = 30;
+$cfg_feedback_numip = 30;
+$cfg_md_mailtest = 'N';
+$cfg_mb_spacesta = -10;
+$cfg_vdcode_member = 'Y';
+$cfg_mb_cktitle = 'Y';
+$cfg_mb_editday = 7;
+$cfg_caicai_sub = 2;
+$cfg_caicai_add = 2;
+$cfg_feedback_add = 5;
+$cfg_feedback_sub = 5;
+$cfg_search_max = 50000;
+$cfg_search_maxrc = 300;
+$cfg_search_time = 3;
+$cfg_baidunews_limit = '100';
+$cfg_updateperi = '15';
+$cfg_sendmail_bysmtp = 'Y';
+$cfg_smtp_server = 'smtp.qq.com';
+$cfg_smtp_port = '25';
+$cfg_smtp_usermail = 'desdev@vip.qq.com';
+$cfg_smtp_user = 'desdev';
+$cfg_smtp_password = '7260444huxiao';
+$cfg_online_type = 'nps';
+$cfg_upload_switch = 'Y';
+$cfg_allsearch_limit = '1';
+$cfg_rewrite = 'N';
+$cfg_delete = 'Y';
+$cfg_keywords = '';
+$cfg_description = '';
+$cfg_beian = '';
+$cfg_need_typeid2 = 'Y';
+$cfg_cache_type = 'id';
+$cfg_max_face = 50;
+$cfg_typedir_df = 'Y';
+$cfg_makeindex = 'N';
+$cfg_make_andcat = 'N';
+$cfg_make_prenext = 'Y';
+$cfg_feedback_forbid = 'N';
+$cfg_jump_once = 'Y';
+$cfg_task_pwd = '';
+$cfg_addon_domainbind = 'N';
+$cfg_addon_domain = '';
+$cfg_df_dutyadmin = 'admin';
+$cfg_mb_allowncarc = 'Y';
+$cfg_mb_allowreg = 'Y';
+$cfg_mb_adminlock = 'N';
+$cfg_sendarc_scores = 10;
+$cfg_sendfb_scores = 3;
+$cfg_mb_spaceallarc = 0;
+$cfg_face_adds = 10;
+$cfg_moreinfo_adds = 20;
+$cfg_money_scores = 50;
+$cfg_mb_wnameone = 'N';
+$cfg_arc_dirname = 'Y';
+$cfg_puccache_time = 36000;
+$cfg_arc_click = -1;
+$cfg_addon_savetype = 'ymd';
+$cfg_qk_uploadlit = 'Y';
+$cfg_login_adds = 2;
+$cfg_userad_adds = 10;
+$cfg_ddimg_full = 'N';
+$cfg_ddimg_bgcolor = 0;
+$cfg_replace_num = 2;
+$cfg_uplitpic_cut = 'Y';
+$cfg_album_mark = 'N';
+$cfg_mb_feedcheck = 'N';
+$cfg_mb_msgischeck = 'N';
+$cfg_mb_reginfo = 'Y';
+$cfg_remote_site = 'N';
+$cfg_title_site = 'N';
+$cfg_mysql_type = 'mysql';
+$cfg_sphinx_article = 'N';
+$cfg_sphinx_host = 'localhost';
+$cfg_sphinx_port = 9312;
+$cfg_memcache_enable = 'N';
+$cfg_memcache_mc_defa = 'memcache://127.0.0.1:11211/default127';
+$cfg_memcache_mc_oth = '';
+$cfg_cross_sectypeid = 'N';
+$cfg_digg_update = 0;
+$cfg_feedback_guest = 'N';
+$lxfs_dianhua = '<p>咨询热线</p>
+<p>13776955589 袁经理</p>';
+$lxfs_qq = '<p>联系手机</p>
+<p></p>';
+$lxfs_sj = '<p>联系手机</p>
+<p>113773798331 孙经理</p>';
+$shouye_jj = '富宝贝生态农庄一个被原生态360°包围的绿色空间，远离都市的嘈杂，农庄融入大自然怀抱，是你在喧闹的工作中放松休闲的一片静土。<br>农庄内设采摘区、垂钓区、田园果蔬区、活动区、摸鱼区、烧烤野炊等多项目娱乐项目，特别还有充满特色的KTV蒙古包。<br>富宝贝是一日游、亲子活动，南通周边游的首选之地。空气清新宜人，绝对是一个放松心情的好地方。';
+$jianjie_bigbang = '骨子里流动着电白人敢于改革、敢于拼搏、敢于开拓的精神，电白建设集团从未忘记迈出向前跨越的步履。海纳百川，谷涵万壑，百尺竿头，更进一步。如今的广东电白建设集团，以广东为依托，用实力、质量、品牌、诚信，将“建筑之乡”的光荣传统发扬光大，让遍布大江南北的琼楼玉宇、康庄大道、碧水绿林记住她湛蓝笃定的身姿。
+
+1979（诞生）
+
+1979年，电白建筑大业萌动发芽。而随着建筑行业的进一步发展，行业也慢慢呈现规范化、规模化的特点，在充满着各种可能性的时代，电白县林头建筑队应运而生。
+
+1986（雏形）
+
+1986年3月，电白在几年间如雨后春笋般纷纷成立建筑队伍，这也象征着电白建筑行业进入了全面生长期。行业需要敢为人先者的探索与追寻，不仅是探索企业组织形式规范化与标准化，更是探索建筑行业的梦想能有多大。电白县林头建筑工程';
+$jianjie_gsmc = '南通富宝贝生态农业';
+$jianjie_gsjja = '位于丛坝村25组334省道旁的富宝贝现代生态循环农业示范园，占地50多亩，常年对外提供商务接待，休闲度假，婚庆礼仪，餐饮垂钓，果蔬采摘，花木盆景，宠物培育，立体种养等服务。';
+$jianjie_gsjjb = '富宝贝生态园以绿色生态环保为特色，餐饮食材都来自员工亲手种植或园内散养，全部实现自给自足
+其中肉禽都是吃着菜叶与粮食长大的，不打激素，所以更加安全放心，
+蔬菜在种植过程中不洒化肥农药，仅靠人工除虫病施以家禽粪便，所以生长旺盛无农药残留';
+$jianjie_gsjjc = '生态园自摘自酿了不少琼浆玉液，无论是自己品尝还是馈赠亲友都是绝佳选择。<br>
+同时生态园内许多半成品食物也是厨师自己制作，如藕饼，虾饼，肉圆，水饺，汤圆，青团等，用料细致，做法正宗，滋味自然可口。<br>';
+$jianjie_gsjjd = ' <p>富宝贝生态园现有客房16间。目前有大床房，套房，标准间等，四合院风格的客房部，静谧雅致古韵天成。</p>
+ <p>在这里既能享受乡村的古朴自然又能有家的温馨舒适。</p>
+ <p>餐饮部则有大厅，小厅，包厢等多种类型，可同时容纳600人用餐，其中大厅可坐满36桌，并安装有音响投影仪等设备，非常适合举办婚宴，生日宴，谢师宴等活动。</p>
+ <p>14间包厢都含有棋牌桌，卫生间等，装饰豪华，服务一流</p>
+ <p></p>';
+?>
